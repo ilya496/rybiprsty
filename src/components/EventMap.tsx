5b42ad1d25fe703b0b82c99526ab89fec669624b
Ilja Novak <birukov0608@gmail.com>
2025-04-09 13:17:02 +0200

    Changed to using Mapy.cz

--- src/components/EventMap.tsx
+++ src/components/EventMap.tsx
@@ -7,6 +7,8 @@ interface EventMapProps {
 }
 
 function EventMap({ position, label }: EventMapProps) {
+  const apiKey = "JA8ynOpU_GgqI0mmgQuGW2mV9M7i6Jv_HLVe3BlfmAo";
+
   return (
     <MapContainer
       center={position}
@@ -15,8 +17,8 @@ function EventMap({ position, label }: EventMapProps) {
       className="event__map"
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
+        url={`https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${apiKey}`}
+        attribution='<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>'
       />
       <Marker position={position}>
         <Popup>{label}</Popup>
