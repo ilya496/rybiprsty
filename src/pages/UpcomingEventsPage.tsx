89594d46041d877b3d438a1c4c35d8517bce2e6b
Ilja Novak <ilya496>
2025-05-07 19:19:06 +0200

    add ticket link

--- src/pages/UpcomingEventsPage.tsx
+++ src/pages/UpcomingEventsPage.tsx
@@ -21,6 +21,8 @@ const events = [
                 Těšíme se na vás.`,
     location: "Praha 1, Nové Město, Národní 116/20 - Rock Café",
     image: "/images/patecek-6-6-2025.jpg",
+    tickets:
+      "https://goout.net/cs/arabsky-patecek-juxtaposition%2Bdalsi/szfxttx/?fbclid=PAZXh0bgNhZW0CMTEAAac4Yg1V1EnDHmrFkIexkOP-qLNf1QeA3dLsJ9SfBslMxlE5m4NJlc0Qw4L5vA_aem_2JpIA8t23L02DRxy51RJ2g",
     mapPosition: [50.0819615, 14.4182511] as [number, number],
   },
   {
